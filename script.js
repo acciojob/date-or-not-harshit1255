2b87d69206eea587d9b174a9baaff6fa36e71092
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-12-02 03:11:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var isDate = function (input) {
   //   write your code here
+
+  return (new Date(input) !== "Invalid Date" &&
+		  !isNaN(new Date(input)));
 };
 
-// Do not change the code below.
+// // Do not change the code below.
 const input = prompt("Enter Date.");
 alert(isDate(input));
